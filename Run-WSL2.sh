f98594be57a862267a75bb741de1bbd932106490
yifdai <92962441+yifdai@users.noreply.github.com>
2025-08-04 03:12:18 -0700

    Update Run.sh

--- Run-WSL2.sh
+++ Run-WSL2.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Define a function to stop the Docker container
 function stop_container() {
-          echo "Stopping genoselect container..."
-            docker stop $container_id
-              echo "Genoselect container stopped."
-      }
+      echo "Stopping genoselect container..."
+      docker stop $container_id
+      echo "Genoselect container stopped."
+}
 
-      # Run the genoselect container and get its ID
-      echo "Starting genoselect container..."
-      container_id=$(docker run -itd -p 4040:4040 -v "$(pwd)":/mount yfd2/ags:1.0.24 /bin/bash)
-      echo "Genoselect container started with ID $container_id"
+# Run the ShinyGS container and get its ID
+echo "Starting genoselect container..."
+container_id=$(docker run -itd -p 4040:4040 -v "$(pwd)":/mount yfd2/ags:1.1.5 /bin/bash)
+echo "Genoselect container started with ID $container_id"
 
-      # Run your R script in the container
 docker exec $container_id Rscript opt/app/run.R &
 
 # Print the Docker container logs
@@ -20,27 +18,28 @@ docker logs -f $container_id &
 
 # Trap the SIGINT signal (ctrl+c) and call the stop_container function
 trap stop_container SIGINT
-
-# Wait for 3 seconds before opening the browser
-sleep 4
+sleep 2
 
 # Open a new browser window and navigate to localhost:4040
-if [ -z "$WSL_DISTRO_NAME" ]; then
-          # This is not a WSL environment, use `open` to start the browser
-            xdg-open http://localhost:4040
-    else
-              # This is a WSL environment, use `cmd.exe` to start the browser
-                cmd.exe /c start http://localhost:4040
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS environment
+    open http://localhost:4040
+elif [ -n "$WSL_DISTRO_NAME" ]; then
+    # WSL environment
+    cmd.exe /c start http://localhost:4040
+else
+    # Standard Linux environment
+    xdg-open http://localhost:4040
 fi
 
 # Listen for the session ended message
 docker logs -f $container_id | while read line ; do
     if echo $line | grep -q "Session ended" ; then
-                    stop_container
-                            break
-                                fi
-                        done
+        stop_container
+        break
+    fi
+done
 
-                        # To ensure the script exits after stopping the container
-                        exit 0
-                        
+# To ensure the script exits after stopping the container
+exit 0
+                      
